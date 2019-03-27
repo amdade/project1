65c44db38613792d72f2dfc39353edd061a727c1
ahmed <amdade76@gmail.com>
2019-03-27 16:47:07 -0500

    initial

--- variable.tf
+++ variable.tf
@@ -10,10 +10,6 @@ variable "public_subnet_cidr" {
   default = "10.0.1.0/24"
 }
 
-variable "private_subnet_cidr" {
-  description = "CIDR for the private subnet"
-  default = "10.0.2.0/24"
-}
 
 variable "ami" {
   description = "Amazon Linux AMI"
--- vpc.tf
+++ vpc.tf
@@ -104,36 +104,5 @@ resource "aws_security_group" "sgweb" {
   }
 }
 
-# Define the security group for private subnet
-resource "aws_security_group" "sgdb"{
-  name = "sg_test_web"
-  description = "Allow traffic from public subnet"
-
-  ingress {
-    from_port = 3306
-    to_port = 3306
-    protocol = "tcp"
-    cidr_blocks = ["${var.public_subnet_cidr}"]
-  }
-
-  ingress {
-    from_port = -1
-    to_port = -1
-    protocol = "icmp"
-    cidr_blocks = ["${var.public_subnet_cidr}"]
-  }
-
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["${var.public_subnet_cidr}"]
-  }
-
-  vpc_id = "${aws_vpc.test-vpc.id}"
-
-  tags {
-    Name = "DB SG"
-  }
   count = 2
 }
