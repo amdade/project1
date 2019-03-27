2da5a017433cebe17eb97766d247f39b9d7a4f07
ahmed <amdade76@gmail.com>
2019-03-27 16:18:18 -0500

    initial

---
+++ variable.tf
@@ -0,0 +1,26 @@
+
+
+variable "vpc_cidr" {
+  description = "CIDR for the VPC"
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR for the public subnet"
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR for the private subnet"
+  default = "10.0.2.0/24"
+}
+
+variable "ami" {
+  description = "Amazon Linux AMI"
+  default = "ami-0de53d8956e8dcf80"
+}
+
+variable "key_name" {
+  description = "SSH Public Key path"
+  default = "project"
+}
