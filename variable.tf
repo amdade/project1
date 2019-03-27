b86ad878691bd5c3b58a7514120ecf46685dcaba
ahmed <amdade76@gmail.com>
2019-03-27 17:05:49 -0500

    initial

--- variable.tf
+++ variable.tf
@@ -9,7 +9,7 @@ variable "public_subnet_cidr" {
   description = "CIDR for the public subnet"
   default = "10.0.1.0/24"
 }
-variable "vpc_security_group_ids" {
+variable "vpc_security_group" {
     default = "vpc_test_web"
 }
 
