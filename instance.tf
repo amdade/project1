4518c0aaeaff76571416e3722082d6e0e0ea84fc
ahmed <amdade76@gmail.com>
2019-03-27 16:57:53 -0500

    initial

--- instance.tf
+++ instance.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "linux" {
    instance_type = "t2.micro"
    key_name = "project"
    subnet_id = "${aws_subnet.public-subnet.id}"
-   vpc_security_group_ids = ["${aws_security_group.sgweb.id}"]
+   vpc_security_group_ids = ["${aws_security_group.vpc_test_web.id}"]
    associate_public_ip_address = true
    source_dest_check = false
    
